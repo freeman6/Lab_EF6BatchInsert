a0e8e159bb90a6c9268905ecd18aabe24d081f62
freeman6 <freeman6@gmail.com>
2020-08-24 22:47:56 +0800

    加入專案檔案。

---
+++ Demo_EFBatchInsert/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_EFBatchInsert
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var testData = GetSample(3000);
+            Demo("Share Context，逐筆 SaveChange", TestRun01, testData);
+            Demo("Not Share Context，逐筆 SaveChange", TestRun02, testData);
+            Demo("Share Context，一次 SaveChange", TestRun03, testData);
+            Demo("Share Context， Batch SaveChange", TestRun04, testData);
+            Demo("Not Share Context， Batch SaveChange", TestRun05, testData);
+            Console.ReadLine();
+        }
+
+        private static IEnumerable<Users> GetSample(int ItemCount)
+        {
+            var randomchar = "0123456789abcdefghijklmnopqrstuvxyzABCDEFGHIJKLMNOPQRSTUVXYZ";
+            var rand = new Random();
+
+            for (int i = 1; i <= ItemCount; i++)
+            {
+                yield return new Users
+                {
+                    UID = i,
+                    Id = i,
+                    Name = new string(Enumerable.Repeat(randomchar, 10).Select(x => x[rand.Next(x.Length)]).ToArray())
+                };
+            }
+        }
+
+        private static void Demo(string title, Action<IEnumerable<Users>> fun, IEnumerable<Users> userData)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            fun(userData);
+            sw.Stop();
+            Console.WriteLine($"資料筆數：{userData.Count()}\t花費時間：{sw.ElapsedMilliseconds}\t測試說明：{title}");
+        }
+
+        /// <summary>
+        /// 逐筆Add，Share Context，逐筆SaveChange
+        /// </summary>
+        /// <param name="ItemData"></param>
+        private static void TestRun01(IEnumerable<Users> ItemData)
+        {
+            TSQL2019Entities dbContext = new TSQL2019Entities();
+
+            foreach (var item in ItemData)
+            {
+                dbContext.Users.Add(item);
+                dbContext.SaveChanges();
+            }
+        }
+
+        /// <summary>
+        /// 逐筆Add，在 Foreach 裡逐筆SaveChange
+        /// </summary>
+        /// <param name="ItemData"></param>
+        private static void TestRun02(IEnumerable<Users> ItemData)
+        {
+            foreach (var item in ItemData)
+            {
+                TSQL2019Entities dbContext = new TSQL2019Entities();
+                dbContext.Users.Add(item);
+                dbContext.SaveChanges();
+            }
+        }
+
+        /// <summary>
+        /// 逐筆Add，一次SaveChange
+        /// </summary>
+        /// <param name="ItemData"></param>
+        private static void TestRun03(IEnumerable<Users> ItemData)
+        {
+            TSQL2019Entities dbContext = new TSQL2019Entities();
+
+            foreach (var item in ItemData)
+            {
+                dbContext.Users.Add(item);
+            }
+            dbContext.SaveChanges();
+        }
+
+        /// <summary>
+        /// 批次Add，批次SaveChange
+        /// </summary>
+        /// <param name="ItemData"></param>
+        private static void TestRun04(IEnumerable<Users> ItemData)
+        {
+            TSQL2019Entities dbContext = new TSQL2019Entities();
+            var batchCount = 100;
+            var i = 0;
+
+            foreach (var item in ItemData)
+            {
+                dbContext.Users.Add(item);
+
+                if (i % batchCount == 0)
+                    dbContext.SaveChanges();
+            }
+            dbContext.SaveChanges();
+        }
+
+        /// <summary>
+        /// 批次Add，批次SaveChange，最後關閉DBContext
+        /// </summary>
+        /// <param name="ItemData"></param>
+        private static void TestRun05(IEnumerable<Users> ItemData)
+        {
+            var batchCount = 100;
+            var skipCount = 0;
+
+            var _ItemData = ItemData.ToArray();
+            for (int i = 1; i <= (ItemData.Count() / batchCount); i++)
+            {
+                using (TSQL2019Entities dbContext = new TSQL2019Entities())
+                {
+                    for (int j = skipCount; j < batchCount * i; j++)
+                    {
+                        dbContext.Users.Add(_ItemData[j]);
+                        skipCount = j+1;
+                    }
+                    dbContext.SaveChanges();
+                }
+            }
+        }
+
+
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,32 @@
+﻿# Demo 比較幾種 Entity Framework 大量新增資料時的效能
+
+### Installation Instructions
+1. 請先安裝 SQL Server Express OR LocalDB
+2. 請執行下述語法建立 Database 及 Table
+```sql
+CREATE DATABASE [TSQL2019]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'TSQL2019', FILENAME = N'C:\Users\reco\TSQL2019.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'TSQL2019_log', FILENAME = N'C:\Users\reco\TSQL2019_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+GO
+
+CREATE TABLE [dbo].[Users](
+	[UID] [int] IDENTITY(1,1) NOT NULL,
+	[Id] [int] NOT NULL,
+	[Name] [nvarchar](50) NULL
+) ON [PRIMARY]
+GO
+```
+
+
+### 校能評比結果(以3000筆資料新增為例)
+| 驗證模式     | 資料筆數的效能 |
+|----------|---------------|
+| 單筆Add，單筆SaveChange | 37562      |
+| 單筆Add，一次SaveChange | 14633      |
+| 批次Add，批次SaveChange | 35784      |
+| 批次Add，批次SaveChange，Dispose dbContext | 13416      |
+
+
